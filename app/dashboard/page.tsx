647550873dba0d27916de9e9d37a7a05f896ff8b
Sameer Ranjan Singh <sameer.lancer99@gmail.com>
2024-11-25 14:24:45 +0530

    clerk authentication, Header, app page and layout setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Sample from '@/components/sample'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Sample name={"Dashboard"}/>
+  )
+}
+
+export default page
---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { PenBox } from "lucide-react"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import UserMenu from "./UserMenu"
+
+export const Header = ()=> {
+    return (
+        <nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
+            <Link href={"/"} className="flex items-center">
+              <Image
+               src={"/orange.png"}
+               width={32}
+               height={32}
+               alt="Appointmate"
+               className="w-auto"
+              />
+              <h1 className="font-bold md:font-extrabold text-xl text-orange-500">AppointMate</h1>
+            </Link>
+
+            <div className="flex items-center gap-4">
+                <Link href={"/events?create=true"}>
+                 <Button className="flex items-center gap-2" variant={"destructive"}><PenBox size={18}/> Create Event</Button>
+                </Link>
+                <SignedOut>
+                 <SignInButton forceRedirectUrl={"/dashboard"}>
+                   <Button className="flex items-center" variant={"outline"}>Login</Button>
+                 </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserMenu/>
+                    {/* <UserButton/> */}
+                </SignedIn>
+            </div>
+        
+        </nav>
+    )
+}
